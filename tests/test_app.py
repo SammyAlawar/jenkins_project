b6a00bfe9766c8ebb8f349c49bb46fd50e0b107d
Sammy Alawar <sna61@mail.aub.edu>
2024-11-14 19:10:59 +0300

    Add unit test for greet function

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,6 @@ import unittest
 from app import greet
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World from Sammy Alawar!"), "Hello, World from Sammy Alawar!")
+        self.assertEqual(greet("World from Sammy Alawar"), "Hello, World from Sammy Alawar!")
 if __name__ == "__main__":
     unittest.main()
